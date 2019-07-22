96a3a9d9a2332b839efd2886894254999c5c98ad
Obiwac <obiwac@gmail.com>
2019-07-22 17:01:20 +0200

    Added checks to the default app

--- code/main.c
+++ code/main.c
@@ -19,7 +19,7 @@ bool main(void) {
 	ui_element_t subtitle;
 	new_ui_element(&subtitle, &ui);
 	ui_element_context_add_element(&context, &subtitle, UI_ELEMENT_CONTEXT_ROLE_SUBTITLE, true, true);
-	ui_element_add_text(&subtitle, ui.fonts[UI_FONT_SUBTITLE], "You have created an AQUA development environment successfully!", CENTER);
+	ui_element_add_text(&subtitle, ui.fonts[UI_FONT_SUBTITLE], "WA /// You have created an AQUA development environment successfully!", CENTER);
 	
 	ui_element_t paragraph;
 	new_ui_element(&paragraph, &ui);
@@ -27,8 +27,30 @@ bool main(void) {
 	ui_element_add_text(&paragraph, ui.fonts[UI_FONT_PARAGRAPH], R"text(Welcome to AQUA.
 If you're reading this message, you have managed to download all the stuff needed to run AQUA,
 and you have managed to successfully compile C code using the AQUA C compiler.
-Yay!)text", CENTER);
+Yay!
+
+Below, you can see what extensions are installed, and what are not.
+Normally, the build script should install everything automatically, but you never know.
+If anything is not installed, don't worry!
+You'll just lose a few features such as certain integrations, request, or sound support.
+
+If you need any help, just ask on my Discord server at https://discord.gg/EVrn2Ka)text", CENTER);
 	
+	ui_element_t checks[3];
+	iterate (sizeof(checks) / sizeof(*checks)) {
+		new_ui_element(&checks[i], &ui);
+		ui_element_context_add_element(&context, &checks[i], UI_ELEMENT_CONTEXT_ROLE_SUBTITLE, true, true);
+		
+	}
+	
+	ui_element_add_text(&checks[0], ui.fonts[UI_FONT_DEFAULT], create_device("requests") ? "CURL is installed"                  : "CURL is not available",                  CENTER);
+	ui_element_add_text(&checks[1], ui.fonts[UI_FONT_DEFAULT], create_device("sound")    ? "PulseAudio is installed"            : "PulseAudio is not available",            CENTER);
+	ui_element_add_text(&checks[2], ui.fonts[UI_FONT_DEFAULT], create_device("discord")  ? "Discord RPC extension is installed" : "Discord RPC extension is not available", CENTER);
+	
+	ui_element_theme_colour(&checks[0], create_device("requests") ? ui.theme.good_colour : ui.theme.warning_colour);
+	ui_element_theme_colour(&checks[1], create_device("sound")    ? ui.theme.good_colour : ui.theme.warning_colour);
+	ui_element_theme_colour(&checks[2], create_device("discord")  ? ui.theme.good_colour : ui.theme.warning_colour);
+		
 	ui_element_layer(&context, 8);
 	ui_element_update(&context);
 	
@@ -49,6 +71,11 @@ Yay!)text", CENTER);
 			ui_resize(&ui);
 			
 		}
+		
+	}
+	
+	iterate (sizeof(checks) / sizeof(*checks)) {
+		dispose_ui_element(&checks[i]);
 		
 	}
 	
