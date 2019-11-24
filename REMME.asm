7e04c27511560d5ae8eebdf1ecda7183f92e9dd7
Obiwac <obiwac@gmail.com>
2019-11-24 17:32:52 +0100

    Tweaks

---
+++ REMME.asm
@@ -0,0 +1,10 @@
+:main:	mov bp sp	sub bp 40###########
+jmp $amber_func_0$end	:$amber_func_0:
+mov g0 0	ret	:$amber_func_0$end:
+cad bp sub 8	mov ?ad $amber_func_0
+mov a0 16	cal malloc	mov a2 a0	mov a0 g0	mov a1 0	cal mset	mov g0 a0	cad bp sub 16	mov ?ad g0
+cad bp sub 16	mov g0 ?ad	cad bp sub 24	mov ?ad g0
+mov g0 1337	cad bp sub 24	cad ?ad add 0	mov ?ad g0	cad bp sub 32	mov ?ad g0
+mov g0 0	cad bp sub 24	cad ?ad add 0	mov ?ad g0	cad bp sub 40	mov ?ad g0
+cad bp sub 24	cad ?ad add 0	mov g0 ?ad	ret
+mov g0 0	ret
